af318dec7bb94bc54f0c8966cc0d7ee025701447
Luis Funes <luiscfunes521@gmail.com>
2022-11-17 16:24:09 -0600

    added a function for loop

--- challenge1.js
+++ challenge1.js
@@ -4,13 +4,17 @@ const list = fs.readFileSync('./users.txt', 'utf8');
 const users = list.split('\n\n')
 
 cont = 0
-for (let index = 0; index < users.length; index++) {
-     if (users[index].includes('usr') && users[index].includes('eme') && users[index].includes('psw') &&users[index].includes('age') && users[index].includes('loc') && users[index].includes('fll')){
-        indexUser = index 
-        cont += 1
-     }
+
+const check = (list) => {
+   for (let index = 0; index < list.length; index++) {
+      if (list[index].includes('usr') && list[index].includes('eme') && list[index].includes('psw') && list[index].includes('age') && list[index].includes('loc') && list[index].includes('fll')){
+         indexUser = index 
+         cont += 1
+      }
+ } 
+   lastUser = list[indexUser]
+   return [cont,lastUser]
 }
 
-lastUser = users[indexUser]
-console.log(cont)
-console.log(lastUser)
+console.log(check(users))
+
